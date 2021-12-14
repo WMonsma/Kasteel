5b7953753e621255dfe84016ea982e96beca2bc2
WMonsma <78416061+WMonsma@users.noreply.github.com>
2021-12-14 14:47:23 +0100

    page navigation

---
+++ kasteel/src/app/modules/bierverrekenen/bierverrekenen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bierverrekenen',
+  templateUrl: './bierverrekenen.component.html',
+  styleUrls: ['./bierverrekenen.component.scss']
+})
+export class BierverrekenenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
